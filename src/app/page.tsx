57738b3df35d2ac28b307a1d7ece8779abcec310
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-04 22:07:15 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    // fragement as per react 
+    <>
+    
+    <h1>Hello next js , how are you ?</h1>
+    
+  </>
+  )
+}
