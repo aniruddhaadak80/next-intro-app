00947e32b12dacfb3cf5b66393fa17f8877f62e4
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-04 22:13:21 +0530

    add mark and h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     // fragement as per react 
     <>
     
-    <h1>Hello next js , how are you ?</h1>
+    <mark>Me </mark> :  <h1>Hello next js , how are you ?</h1>
+   <mark>Next js</mark> : <h2>I am fine bro </h2>
     
   </>
   )
