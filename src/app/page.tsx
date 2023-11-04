d78dcb322b6ac8ecbcc9163cdd72b7a974b05d8c
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-04 22:27:45 +0530

    Add center and br tag in nextr app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,17 @@ export default function Home() {
   return (
     // fragement as per react 
     <>
-    
+
+  <center>
+<br/>
     <mark>Me </mark> :  <h1>Hello next js , how are you ?</h1>
-   <mark>Next js</mark> : <h2>I am fine bro </h2>
+
+<br/>
+
+   <mark>Next js</mark> : <h2>I am fine bro .... </h2>
+
+</center>
+ 
     
   </>
   )
