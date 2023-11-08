b23a8ed8bc47d93763ac3c1cd26e9b048d319499
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-09 00:21:12 +0530

    / add in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 
     <html lang="en">
     <head>
-        <link rel='icon' href='favicon.jpg'/>
+        <link rel='icon' href='/favicon.jpg'/>
       </head>
 
       <body>{children}</body>
