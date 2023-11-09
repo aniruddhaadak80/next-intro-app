76b32c5e9b12254cb1f36bcab8d99b07d2c11176
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-09 09:00:31 +0530

    Add not-found.js in src folder to show users 404 error if the routes are unavailble

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function Not_found(){
+    return(
+        <>
+            <h1>This page is currently Unavailable</h1>
+            <img src="https://www.gstatic.com/youtube/src/web/htdocs/img/monkey.png" alt='monkey image'/>
+        </>
+    )
+}
